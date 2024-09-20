676cd06fd79d4cd45e5a200aafb23eade3b29c2f
Fernando Sales <lfgsnando@gmail.com>
2024-09-20 00:47:31 -0300

    Create function to search with query

--- app/lib/api/http_client.dart
+++ app/lib/api/http_client.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
-import 'package:dio/io.dart';
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:marvel_app/api/paths.dart';
 import 'package:marvel_app/model/character.dart';
 
@@ -12,15 +12,45 @@ final _baseOptions = BaseOptions(
   receiveTimeout: const Duration(milliseconds: 30000)
 );
 
+final _cacheOptions = CacheOptions(
+  store: MemCacheStore(),
+  policy: CachePolicy.request,
+  hitCacheOnErrorExcept: [401, 403],
+  maxStale: const Duration(minutes: 10),
+  priority: CachePriority.normal,
+);
+
 class HttpClient  {
   Dio dio = Dio();
   HttpClient() {
     dio = Dio(_baseOptions);
+    dio.interceptors.add(DioCacheInterceptor(options: _cacheOptions));
   }
 
   Future<List<Character>> fetchCharacters() async {
     try {
       final response = await dio.get('$charactersListEndpont?ts=1&apikey=5594c280f80182b94f0ef948a42467c1&hash=e297bab9d570364275a1507ea76b39fb');
+      if (response.statusCode == 200) {
+        final data = response.data['data']['results'] as List<dynamic>;
+        List<Character> characters = data.map((characterData) {
+          return Character(
+            id: characterData['id'], 
+            name: characterData['name'], 
+            description: characterData['description'],
+            thumbnail: characterData['thumbnail']['path']+ '.' + characterData['thumbnail']['extension']);
+        }).toList();
+        return characters;
+      } else {
+        throw Exception('Failed to load characters');
+      }
+    } catch (e) {
+      throw Exception('Failed to load characters $e');
+    }
+  }
+
+  Future<List<Character>> fetchCharactersNameStartsWith(String name) async {
+    try {
+      final response = await dio.get('$charactersListEndpont?nameStartsWith=$name&ts=1&apikey=5594c280f80182b94f0ef948a42467c1&hash=e297bab9d570364275a1507ea76b39fb');
       if (response.statusCode == 200) {
         final data = response.data['data']['results'] as List<dynamic>;
         print(data);
